fe902c13c9198dc704abd369beab7a482407abcc
a5h3 <boxer_radian.0r@icloud.com>
2026-02-08 14:08:27 -0700

    partial complete 'Initial Database Setup'

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 ## update log
 
+### 020826
+#### Submission: [Capstone Project: Initial Database Setup]
+- Partial complete part 1 based on progress from the class.
+  * Model structure, makemigration
+- Will be updated through part 3 after class demo related.
 ### 020126
 #### Submission: [Capstone Project: Static Style and Data Blueprint]
 ### 012526
--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,44 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+class Condition(models.TextChoices):
+    NEW = 'new', 'New'
+    EXCELLENT = 'excellent', 'Excellent'
+    GOOD = 'good', 'Good'
+    FAIR = 'fair', 'Fair'
+    POOR = 'poor', 'Poor'
+
+class Location(models.TextChoices):
+    SU = 'shepherd-union', 'Shepherd Union',
+    LI = 'steward-library', 'Steward Library',
+    NB = 'noorda-building', 'Noorda Building',
+    TY = 'tracy-hall', 'Tracy Hall',
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.CharField(max_length=50)
+    condition = models.CharField(
+        max_length=10,
+        choices=Condition.choices,
+        default=Condition.GOOD,
+    )
+    location = models.CharField(
+        max_length=20,
+        choices=Location.choices,
+        default=Location.SU,
+    )
+    available = models.BooleanField(default=True)
+    postedAt = models.DateTimeField(auto_now_add=True)
+    seller = models.CharField(max_length=50)
+    image = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} | ${self.price}"
+
+
+
+
